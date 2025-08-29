ab4b7d5c825025af162c3984864385beb4d6072f
clahwerck <isurialwis132@gmail.com>
2025-08-29 15:42:32 +0530

    refactor: restructure routing to use singleton pattern for routes

--- src/routes/greeting.route.ts
+++ src/routes/greeting.route.ts
@@ -1,15 +1,33 @@
-import { Router, Request, Response } from "express";
+ import { Router, Request, Response } from "express";
 
-const router = Router();
+export class GreetingRoute{
+    private static instance: GreetingRoute;
+    public static getInstance():GreetingRoute{
+        if(!GreetingRoute.instance){
+            GreetingRoute.instance = new GreetingRoute();
+        }
+        
+        return GreetingRoute.instance;
 
-router.get("/", (req: Request, res: Response) => {
-    res.send("Hello World");
-});
+    }
+    public router:Router;
+    private constructor(){
+        this.router = Router();
+        this.setupRoutes();     
 
-router.post("/", (req: Request, res: Response) => {
-  const { name } = req.body.name = req.body.name;
-  res.send(`Hello ${name}`);
-});
+    }
+
+    private setupRoutes(){
+       this.router.get("/", (req: Request, res: Response) => {
+           res.send("Hello World"); 
+       });
+
+       this.router.post("/", (req: Request, res: Response) => {
+           const { name } = req.body.name = req.body.name;
+           res.send(`Hello ${name}`);
+       });  
+
+    }
+}
+
   //Postman? : Great for debugging your backend before connecting it to a frontend.
-
- export default router;
